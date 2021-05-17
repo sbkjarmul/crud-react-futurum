eb04c06a80a500698ef2e49955e2137660aed64f
sbkjarmul <37276167+sbkjarmul@users.noreply.github.com>
2021-05-17 15:25:21 +0200

    add react-icons

--- src/components/organisms/Navigation.js
+++ src/components/organisms/Navigation.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../../App.css'
 
 const Navigation = () => {
   return (
-    <div>
-      <Link to='/'>Home</Link>
-      <Link to='/add'>Add Campaign</Link>
+    <div className='navbar' >
+      <Link className='navbar__link' to='/'>Home</Link>
+      <Link className='navbar__link' to='/add'>Add</Link>
     </div>
   )
 }
