2a355b034f0467fb4d9d6a97b8bf09c5791798cd
sbkjarmul <37276167+sbkjarmul@users.noreply.github.com>
2021-05-17 14:51:03 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
+import Home from './components/templates/Home'
+import AddCampaign from './components/templates/AddCampaign'
+import EditCampaign from './components/templates/EditCampaign'
+import Navigation from './components/organisms/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navigation></Navigation>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <AddCampaign path='/add' component={AddCampaign} />
+          <EditCampaign path='/edit/:id' component={EditCampaign} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/templates/EditCampaign.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditCampaign = () => {
+  return (
+    <div>
+      <h1>Edit Campaign</h1>
+    </div>
+  )
+}
+
+export default EditCampaign;
